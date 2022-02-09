c1aeb9e08bea50d2e997b1ab90f5b4a1d614fc36
jbarrette9214 <jpb0800@yahoo.com>
2022-02-08 20:57:45 -0500

    initial maven commit

---
+++ src/main/java/com/barrette/f1site/F1backend1Application.java
@@ -0,0 +1,13 @@
+package com.barrette.f1site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1backend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1backend1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/barrette/f1site/F1backend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barrette.f1site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1backend1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
